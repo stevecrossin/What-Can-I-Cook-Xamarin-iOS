a8c9b78ccfb4998bd4bc46fb2c6e35142608417f
Brendon Binks <bjbinks@deakin.edu.au>
2019-08-28 20:26:11 +1000

    Added Ingredient, Intolerance, Pantry, Recipe, RecipeIngredients, RecipeIngredientsTotal and User entities

---
+++ WhatCanICook/Entities/Intolerance.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace WhatCanICook.Entities
+{
+    public class Intolerance
+    {
+
+        /**********************
+         VARIABLES
+         *********************/
+
+        private int intoleranceID;
+        private String intoleranceName;
+        private String ingredientName;
+        private bool intoleranceSelected;
+
+        /**********************
+         PROPERTIES
+         *********************/
+
+        //Properties to expose variables as opposed to getter and setter methods
+
+        public int IntoleranceID
+        {
+            set { intoleranceID = value; }
+        }
+
+        public String IntoleranceName
+        {
+            get { return intoleranceName; }
+            set { intoleranceName = value; }
+        }
+
+        public String IngredientName
+        {
+            get { return ingredientName; }
+            set { ingredientName = value; }
+        }
+
+        public bool IntoleranceSelected
+        {
+            get { return intoleranceSelected; }
+            set { intoleranceSelected = value; }
+        }
+
+        /**********************
+         METHODS
+         *********************/
+
+        //Constructor
+        public Intolerance(String intoleranceName, String ingredientName)
+        {
+            this.intoleranceName = intoleranceName;
+            this.ingredientName = ingredientName;
+            this.intoleranceSelected = false;
+        }
+    }
+}
---
+++ WhatCanICook/Entities/RecipeIngredientsTotal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace WhatCanICook.Entities
+{
+    public class RecipeIngredientsTotal
+    {
+
+        /**********************
+         VARIABLES
+         *********************/
+
+        private int id;
+        private String recipeName;
+        private int totalIngredients;
+
+        /**********************
+         PROPERTIES
+         *********************/
+
+        //Properties to expose variables as opposed to getter and setter methods
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String RecipeName
+        {
+            get { return recipeName; }
+            set { recipeName = value; }
+        }
+
+        public int TotalIngredients
+        {
+            get { return totalIngredients; }
+            set { totalIngredients = value; }
+        }
+
+        /**********************
+         METHODS
+         *********************/
+
+        //Constructor
+        public RecipeIngredientsTotal(String recipeName, int totalIngredients)
+        {
+            this.recipeName = recipeName;
+            this.totalIngredients = totalIngredients;
+        }
+    }
+}
