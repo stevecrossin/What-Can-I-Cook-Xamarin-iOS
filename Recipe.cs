439372f59030eda10ba68a586cbb3fe798097a06
Steve Crossin <cro0016@gmail.com>
2019-08-31 13:03:59 +1000

    Upload Entities

---
+++ Recipe.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace WhatCanICook.Entities
+{
+    public class Recipe
+    {
+
+        /**********************
+         VARIABLES
+         *********************/
+
+        private int recipeId;
+        private String recipeName;
+        private String recipeImage;
+        private String recipeIngredients;
+        private String recipeSteps;
+        private int recipeExcluded = 0;
+        private bool isSaved;
+        private bool isCustomed;
+
+        /**********************
+         PROPERTIES
+         *********************/
+
+        //Properties to expose variables as opposed to getter and setter methods
+        public int RecipeID
+        {
+            set { RecipeID = value; }
+        }
+
+        public String RecipeName
+        {
+            get { return recipeName; }
+            set { recipeName = value; }
+        }
+
+        public String RecipeImage
+        {
+            get { return recipeImage; }
+            set { recipeImage = value; }
+        }
+
+        public String RecipeIngredients
+        {
+            get { return recipeIngredients; }
+            set { recipeIngredients = value; }
+        }
+
+        public String RecipeSteps
+        {
+            get { return recipeSteps; }
+            set { recipeSteps = value; }
+        }
+
+        public int RecipeExcluded
+        {
+            get { return recipeExcluded; }
+            set { recipeExcluded = value; }
+        }
+
+        public bool IsSaved
+        {
+            get { return isSaved; }
+            set { isSaved = value; }
+        }
+
+        public bool IsCustomed
+        {
+            get { return isCustomed; }
+            set { isCustomed = value; }
+        }
+
+        /**********************
+         METHODS
+         *********************/
+
+        //Constructor
+        public Recipe(String recipeName, String recipeImage, String recipeIngredients, String recipeSteps)
+        {
+            this.recipeName = recipeName;
+            this.recipeImage = recipeImage;
+            this.recipeIngredients = recipeIngredients;
+            this.recipeSteps = recipeSteps;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace WhatCanICook.Entities
+{
+    public class User
+    {
+        /**********************
+         VARIABLES
+         *********************/
+
+        private int userID;
+        private String userName;
+        private String passKey;
+        private String intolerances;
+        private bool isLoggedIn;
+        private String savedIngredients;
+
+        /**********************
+         PROPERTIES
+         *********************/
+
+        //Properties to expose variables as opposed to getter and setter methods
+        public int UsedID
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+
+        public String UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        public String PassKey
+        {
+            get { return passKey; }
+            set { passKey = value; }
+        }
+
+        public String Intolerances
+        {
+            get { return intolerances; }
+            set { intolerances = value; }
+        }
+
+        public bool IsLoggedIn
+        {
+            get { return isLoggedIn; }
+            set { isLoggedIn = value; }
+        }
+
+        public String SavedIngredients
+        {
+            get { return savedIngredients; }
+            set { savedIngredients = value; }
+        }
+
+        /**********************
+         METHODS
+         *********************/
+
+        //Constructor
+        public User(String userName, String passKey)
+        {
+            this.userName = userName;
+            this.passKey = passKey;
+            this.isLoggedIn = false;
+            this.intolerances = "[]";
+            this.savedIngredients = "[]";
+        }
+    }
+}
