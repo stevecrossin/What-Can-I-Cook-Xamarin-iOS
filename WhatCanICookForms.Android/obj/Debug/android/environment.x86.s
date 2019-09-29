89ec10a8d9cd53045ad50a7c52f22571fd0aa503
Steven Crossin <stevecrossin@gmail.com>
2019-09-29 10:06:37 +0930

    Fixing issue where Android splash screen broke display size.

--- WhatCanICookForms.Android/obj/Debug/android/environment.x86.s
+++ WhatCanICookForms.Android/obj/Debug/android/environment.x86.s
@@ -13,20 +13,17 @@ application_config:
 	.byte	0
 	/* uses_mono_aot */
 	.byte	0
-	/* uses_embedded_dsos */
-	.byte	0
 	/* uses_assembly_preload */
 	.byte	1
 	/* is_a_bundled_app */
 	.byte	0
 	/* environment_variable_count */
-	.zero	3
 	.long	10
 	/* system_property_count */
 	.long	0
 	/* android_package_name */
 	.long	.L.str.1
-	.size	application_config, 20
+	.size	application_config, 16
 	.section	.rodata..L.str.2,"aMS",@progbits,1
 	.type	.L.str.2, @object
 .L.str.2:
@@ -64,7 +61,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"4e1f54f3-02cb-4bdd-a8bf-d3d13f343e99"
+	.asciz	"74acd527-baa5-426b-be90-5325bf9c95a4"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
